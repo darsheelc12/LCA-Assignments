875dddabee4f901c6936fd320fc6add80fe88604
darsheelc12 <darshilchhajed2008@gmail.com>
2026-09-26 22:45:25 +0530

    Refactor string validation using regex

--- #LCA - ASSIGNMENT 5.py
+++ #LCA - ASSIGNMENT 5.py
@@ -1,16 +1,9 @@
-#checking that a string contains only a certain set of characters (in this case a-z, A-Z and 0-9). 
+ # checking that a string contains only a certain set of characters (in this case a-z, A-Z and 0-9). 
+import re
+
 s = input("Enter the string: ")
 
-valid = True
-
-for char in s:
-    if char.isalpha() or char.isdigit():
-        valid = True
-    else:
-        valid = False
-        break
-
-if valid:
+if re.fullmatch(r"[a-zA-Z0-9]+", s):
     print("String accepted")
 else:
     print("String rejected")
